c6ce6f85ae458235fe9e7ea78370bdc018398fc6
unknown <iago.siccardi@gmail.com>
2022-04-16 19:59:55 -0300

    formularios 50%

--- app.js
+++ app.js
@@ -9,4 +9,13 @@ app.get("/",(req,res) => {
     return res.sendFile(path.join(__dirname,'views','home.html'))
 })
 
+
+app.get("/register",(req,res) => {
+    return res.sendFile(path.join(__dirname,'views','register.html'))
+})
+
+app.get("/login",(req,res) => {
+    return res.sendFile(path.join(__dirname,'views','login.html'))
+})
+
 app.listen(port,() => console.log(`Servidor corriendo en http://localhost:${port}`))
